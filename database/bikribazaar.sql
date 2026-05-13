e68aaccaa71e265dc4bec0b4eba265cb089fcf77
pathakharsh584-bit <pathakharsh584@gmail.com>
2026-05-13 15:06:12 +0530

    updated bikribazaar.sql

--- database/bikribazaar.sql
+++ database/bikribazaar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 11, 2026 at 05:03 PM
+-- Generation Time: May 13, 2026 at 11:32 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,9 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `favorites` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `product_id` int(11) DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `favorites`
+--
+
+INSERT INTO `favorites` (`id`, `user_id`, `product_id`, `created_at`) VALUES
+(1, 1, 2, '2026-05-12 10:42:23');
 
 -- --------------------------------------------------------
 
@@ -41,21 +49,24 @@ CREATE TABLE `favorites` (
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `description` text DEFAULT NULL,
-  `price` varchar(100) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `risk_level` varchar(50) DEFAULT NULL
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
 --
 
-INSERT INTO `products` (`id`, `user_id`, `title`, `description`, `price`, `image`, `created_at`, `risk_level`) VALUES
-(6, 1, 'robot', '3 month old', '300', 'Screenshot 2026-04-23 194116.png', '2026-05-10 08:56:45', NULL);
+INSERT INTO `products` (`id`, `user_id`, `title`, `description`, `price`, `location`, `category`, `image`, `created_at`) VALUES
+(1, 1, 'dsgds', 'dsGsd', 15000.00, 'gdsgds', 'Mobiles', '1778519721_Screenshot 2026-04-10 163542.png', '2026-05-11 17:15:21'),
+(2, 1, 'LAPTOP', 'NEW LAPTOP ONLY ONE MONTH USED', 20000.00, 'JHARKHAND', 'Electronics', '1778575231_Screenshot 2026-04-23 192935.png', '2026-05-12 08:40:31'),
+(3, 1, 'tv', '3 month', 20000.00, 'JHARKHAND', 'Electronics', '1778648196_Screenshot 2026-04-23 194101.png', '2026-05-13 04:56:36');
 
 -- --------------------------------------------------------
 
@@ -71,16 +82,19 @@ CREATE TABLE `users` (
   `phone` varchar(20) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `reset_token` varchar(64) DEFAULT NULL,
-  `reset_token_expiry` datetime DEFAULT NULL
+  `otp` varchar(10) DEFAULT NULL,
+  `otp_expiry` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `city`, `created_at`) VALUES
-(1, 'Harsh Pathak', 'pathakharsh584@gmail.com', '$2y$10$eu890BC/KyEbszqtgihBT.96osa/mCkkYq0p8bgHSD88MfLtiW/be', NULL, NULL, '2026-05-10 08:00:42');
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `phone`, `city`, `created_at`, `otp`, `otp_expiry`) VALUES
+(1, 'Harsh Pathak', 'pathakharsh584@gmail.com', '$2y$10$YJHt8By.r8grvXSjw9OYMOgtf39MCaAZa3IY5WJjttei.YQcUysw2', NULL, NULL, '2026-05-10 08:00:42', NULL, NULL),
+(2, 'Harsh Pathak', 'pathakneetu584@gmail.com', '$2y$10$uHIntx3lfjhTmLPiRcJHGOUaPQJPT/cQ9XKiLsQ7U75A7JhakUaB.', '09229214413', 'JAMSHEDPUR', '2026-05-11 16:29:37', NULL, NULL),
+(3, 'Harsh Pathak', 'pathakneet584@gmail.com', '$2y$10$AHKYYL2PUicyE4fqLwFK9.ryF9cMHGns0H4s/5olA0qEEvBDBxCQO', '09229214413', 'JAMSHEDPUR', '2026-05-12 15:50:30', NULL, NULL),
+(4, 'Jaspreet', 'jaspreetsk.2020@gmail.com', '$2y$10$Q6M1TUIjblY87TSSMmw0Lu/Lf0dJ7RGb5yEu.bK9BtuqEfBTfNSIq', '9234241658', 'JAMSHEDPUR', '2026-05-12 17:58:06', '385115', '2026-05-12 20:14:03');
 
 --
 -- Indexes for dumped tables
@@ -112,19 +126,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `favorites`
 --
 ALTER TABLE `favorites`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
